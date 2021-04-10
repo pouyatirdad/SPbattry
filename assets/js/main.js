32b2959178d6a50a8c45588d66dacfd0c688607e
pouya <pouyatirdad70@gmail.com>
2021-04-10 11:24:08 +0430

    fix mobille nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -575,3 +575,16 @@ function deleteInCart() {
     Cartnum.innerHTML = CartNumbuy;
 
 }
+
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
